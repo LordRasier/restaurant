68e7cc081c739a597249ad1b3f6368d97c1237fa
kellian <kellian.rea@groobyte.com>
2019-11-19 17:16:03 -0300

    update

---
+++ mesa2.js
@@ -0,0 +1,12 @@
+var mesa = (function(user,pass,contenedor){
+    var usuario = user;
+    var pass = pass;
+    var contenedor = contenedor;
+    var idleTime = 60;
+    var opciones = [];
+    var lista = [];
+
+    var init = function(){
+        
+    }
+});
